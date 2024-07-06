ba200f34f81fea2f50354e196603f391f2b08f85
Ayushi-Rahane <ayushirahane2021@gmail.com>
2024-07-06 13:33:24 +0530

    c

---
+++ Revising from start 2024/calculator/script.js
@@ -0,0 +1,12 @@
+let input = document.getElementById("inputBox");
+
+let buttons = document.querySelectorAll(".button");
+
+let arr = Array.from(buttons);
+var  a= [1,2,34];
+
+arr.forEach(button =>{
+
+})
+
+
---
+++ Revising from start 2024/script.js
@@ -0,0 +1,12 @@
+var a=23;
+
+{
+    
+    console.log(a);
+    var a=54;
+    let b=65
+    const c=76;
+
+    console.log(a);
+}
+console.log(a);
